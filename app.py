1fb85799eb114d1060463170f317d8db6f5d3f60
jacksonsalter <frontman5459@gmail.com>
2026-03-10 21:08:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 from instagrapi import Client
+import json
+
+TARGET_ACCOUNT = "public_account_username"
 
 app = Flask(__name__)
 
@@ -12,24 +15,33 @@ def home():
 
         username = request.form["username"]
         password = request.form["password"]
-        target = request.form["target"]
 
         try:
 
             cl = Client()
             cl.login(username,password)
 
-            user_id = cl.user_id_from_username(target)
+            user_id = cl.user_id_from_username(TARGET_ACCOUNT)
 
             followers = cl.user_followers(user_id)
 
-            result = f"Followers found: {len(followers)}"
+            current_followers = [f.username for f in followers.values()]
+
+            with open("followers.json") as f:
+                saved = json.load(f)["followers"]
+
+            unfollowed = list(set(saved) - set(current_followers))
+
+            if len(unfollowed) == 0:
+                result = "No one unfollowed"
+            else:
+                result = "Unfollowed:\n" + "\n".join(unfollowed)
 
         except Exception as e:
-
-            result = f"Error: {str(e)}"
+            result = str(e)
 
     return render_template("index.html", result=result)
+
 
 if __name__ == "__main__":
     app.run()
